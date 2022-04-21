71aeca40bde3560e8f962c7157f1c3c74ec319db
Rafiki022 <rase.cloud7@gmail.com>
2022-04-21 15:06:07 +0200

    Cambio radical

--- app/src/main/java/com/example/what_a_hole/MainActivity.java
+++ app/src/main/java/com/example/what_a_hole/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*comentario prueba*/
+        /*comentario pruebgwrq gq 3rqw ra*/
     }
 }
